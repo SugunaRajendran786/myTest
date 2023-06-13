e61916d9f482bd1aac133caaa6e8c5bfaf6102ee
Suguna Rajendran <sugurajan0904@gmail.com>
2023-06-13 21:18:47 +0530

    first commit

---
+++ DemoClass.java
@@ -0,0 +1,12 @@
+package com.test.packages;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="AutoDownload";
+	 	System.out.println("string length is: "+s1.length());//12 is the length of s1 string  
+		
+	}
+
+}
